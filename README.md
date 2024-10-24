69744facff0b3678253ebd8aa9e0b481c1b8752b
amithcabraal <git@stackblitz.com>
2024-10-24 05:40:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Countries-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/amithcabraal/Countries-)
---
+++ src/data/countries.ts
@@ -0,0 +1,107 @@
+export const countries = [
+  {
+    name: 'Japan',
+    flag: 'https://flagcdn.com/w640/jp.png',
+    population: 125700000,
+    capital: 'Tokyo',
+    mainExport: 'Vehicles'
+  },
+  {
+    name: 'Brazil',
+    flag: 'https://flagcdn.com/w640/br.png',
+    population: 214300000,
+    capital: 'Brasília',
+    mainExport: 'Soybeans'
+  },
+  {
+    name: 'Germany',
+    flag: 'https://flagcdn.com/w640/de.png',
+    population: 83200000,
+    capital: 'Berlin',
+    mainExport: 'Automobiles'
+  },
+  {
+    name: 'Australia',
+    flag: 'https://flagcdn.com/w640/au.png',
+    population: 25690000,
+    capital: 'Canberra',
+    mainExport: 'Iron Ore'
+  },
+  {
+    name: 'India',
+    flag: 'https://flagcdn.com/w640/in.png',
+    population: 1408000000,
+    capital: 'New Delhi',
+    mainExport: 'Petroleum Products'
+  },
+  {
+    name: 'Canada',
+    flag: 'https://flagcdn.com/w640/ca.png',
+    population: 38250000,
+    capital: 'Ottawa',
+    mainExport: 'Oil'
+  },
+  {
+    name: 'France',
+    flag: 'https://flagcdn.com/w640/fr.png',
+    population: 67390000,
+    capital: 'Paris',
+    mainExport: 'Aircraft'
+  },
+  {
+    name: 'Mexico',
+    flag: 'https://flagcdn.com/w640/mx.png',
+    population: 128900000,
+    capital: 'Mexico City',
+    mainExport: 'Vehicles'
+  },
+  {
+    name: 'Spain',
+    flag: 'https://flagcdn.com/w640/es.png',
+    population: 47350000,
+    capital: 'Madrid',
+    mainExport: 'Vehicles'
+  },
+  {
+    name: 'Italy',
+    flag: 'https://flagcdn.com/w640/it.png',
+    population: 59550000,
+    capital: 'Rome',
+    mainExport: 'Machinery'
+  },
+  {
+    name: 'Netherlands',
+    flag: 'https://flagcdn.com/w640/nl.png',
+    population: 17440000,
+    capital: 'Amsterdam',
+    mainExport: 'Machinery'
+  },
+  {
+    name: 'Sweden',
+    flag: 'https://flagcdn.com/w640/se.png',
+    population: 10350000,
+    capital: 'Stockholm',
+    mainExport: 'Machinery'
+  },
+  {
+    name: 'Singapore',
+    flag: 'https://flagcdn.com/w640/sg.png',
+    population: 5686000,
+    capital: 'Singapore',
+    mainExport: 'Electronics'
+  },
+  {
+    name: 'Argentina',
+    flag: 'https://flagcdn.com/w640/ar.png',
+    population: 45195000,
+    capital: 'Buenos Aires',
+    mainExport: 'Soybeans'
+  },
+  {
+    name: 'Thailand',
+    flag: 'https://flagcdn.com/w640/th.png',
+    population: 69950000,
+    capital: 'Bangkok',
+    mainExport: 'Electronics'
+  }
+];
